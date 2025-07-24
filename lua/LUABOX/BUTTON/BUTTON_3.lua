2a512acc5ff10bf308b445236bc5a411e1784969
lanyizi <lanyi@ra3.moe>
2025-07-24 13:44:05 +0200

    修正编码

--- lua/LUABOX/BUTTON/BUTTON_3.lua
+++ lua/LUABOX/BUTTON/BUTTON_3.lua
@@ -1,26 +1,26 @@
 function onUserLongTextDialogEvent(playerName, btnIndex, dialogId)
   for i=1,6,1 do
-  if playerName=="Player_"..i and i<=3 and dialogId==200+i then
-    if btnIndex==1 then
-      exAddTextToPublicBoard('鎭堕瓟$p'..i..'Name閫夋嫨鎶曢檷',30)
-      devil_surrender=devil_surrender+1
-    elseif btnIndex==3 then
-      exAddTextToPublicBoard('鎭堕瓟$p'..i..'Name閫夋嫨鎴樻枟',30)
-      devil_surrender=devil_surrender-1
+    if playerName=="Player_"..i and i<=3 and dialogId==200+i then
+      if btnIndex==1 then
+        exAddTextToPublicBoard('恶魔$p'..i..'Name选择投降',30)
+        devil_surrender=devil_surrender+1
+      elseif btnIndex==3 then
+        exAddTextToPublicBoard('恶魔$p'..i..'Name选择战斗',30)
+        devil_surrender=devil_surrender-1
       else
-      exAddTextToPublicBoard('鎭堕瓟$p'..i..'Name閫夋嫨寮冩潈',30)
+        exAddTextToPublicBoard('恶魔$p'..i..'Name选择弃权',30)
+      end
+    end
+    if playerName=="Player_"..i and i>3 and dialogId==200+i then
+      if btnIndex==1 then
+        exAddTextToPublicBoard('天使$p'..i..'Name选择投降',30)
+        angel_surrender=angel_surrender+1
+      elseif btnIndex==3 then
+        exAddTextToPublicBoard('天使$p'..i..'Name选择战斗',30)
+        angel_surrender=angel_surrender-1
+      else
+        exAddTextToPublicBoard('天使$p'..i..'Name选择弃权',30)
+      end
     end
   end
-  if playerName=="Player_"..i and i>3 and dialogId==200+i then
-    if btnIndex==1 then
-    exAddTextToPublicBoard('澶╀娇$p'..i..'Name閫夋嫨鎶曢檷',30)
-      angel_surrender=angel_surrender+1
-    elseif btnIndex==3 then
-    exAddTextToPublicBoard('澶╀娇$p'..i..'Name閫夋嫨鎴樻枟',30)
-      angel_surrender=angel_surrender-1
-      else
-    exAddTextToPublicBoard('澶╀娇$p'..i..'Name閫夋嫨寮冩潈',30)
-    end
-  end
-end
 end
