8d9f50696d1a4c53761b53cc09d73ce7372a19d8
lanyizi <lanyi@ra3.moe>
2025-07-25 15:20:36 +0200

    修

--- lua/GAME/CelestialBatteryDamaged.lua
+++ lua/GAME/CelestialBatteryDamaged.lua
@@ -8,10 +8,14 @@ FilterCelestialBattery=CreateObjectFilter({
 
 SchedulerModule.call_every_x_frame(function()
     local obj, count = ObjectFindObjects(nil, nil, FilterCelestialBattery)
+    local playerCounts = {}
     for i = 1 , count , 1 do
-        if i <= 3 then
+        local player = ObjectPlayerScriptName(obj[i])
+        local playerCount = playerCounts[player] or 1
+        if playerCount <= 3 then
             ExecuteAction("NAMED_DAMAGE", obj[i], 8);
         end
+        playerCounts[player] = playerCount + 1
     end
 
 end, 30, nil)
