125e43a50fc566785d312b673e4a11a77b9b2314
lanyizi <lanyi@ra3.moe>
2026-07-20 14:27:26 +0200

    尝试修复夜莺bug

--- lua/GAME/EnvKillAllUnit.lua
+++ lua/GAME/EnvKillAllUnit.lua
@@ -12,6 +12,12 @@ unitallEnv = CreateObjectFilter({
     Include="SELECTABLE",
     Exclude="STRUCTURE",
 })
+-- 夜莺也需要被干掉
+killableNightinale = CreateObjectFilter({
+    Relationship="SAME_PLAYER",
+    IncludeThing={ "AlliedNightinaleHelicopter" },
+    StatusBitFlagsExclude="STEALTHED",
+})
 
 RoundLuaManager.CallOnEveryRoundBegin(function(list)
     local level = exCounterGetByName("lvc")
@@ -21,14 +27,24 @@ RoundLuaManager.CallOnEveryRoundBegin(function(list)
         end
         if level == 9 or level == 23 then
             exAddTextToPublicBoard(Localization.get("env_kill_all_unit.execute"), 10)
-            local units, count = ObjectFindObjects(T74, nil,unitallEnv);
+            local units, count = ObjectFindObjects(T74, nil, unitallEnv);
             for i = 1 , count , 1 do
                 ExecuteAction("NAMED_KILL", units[i]);
             end
 
-            local units2, count2 = ObjectFindObjects(T84, nil,unitallEnv);
+            local units2, count2 = ObjectFindObjects(T84, nil, unitallEnv);
             for i = 1 , count2 , 1 do
                 ExecuteAction("NAMED_KILL", units2[i]);
+            end
+
+            local nightinaleUnits, nightinaleCount = ObjectFindObjects(T74, nil, killableNightinale)
+            for i = 1 , nightinaleCount , 1 do
+                ExecuteAction("NAMED_KILL", nightinaleUnits[i]);
+            end
+
+            local nightinaleUnits2, nightinaleCount2 = ObjectFindObjects(T84, nil, killableNightinale)
+            for i = 1 , nightinaleCount2 , 1 do
+                ExecuteAction("NAMED_KILL", nightinaleUnits2[i]);
             end
         end
     end
--- lua/LUABOX/HIDEINI_0.lua
+++ lua/LUABOX/HIDEINI_0.lua
@@ -1,7 +1,13 @@
 FilterJapanLightTransportVehicle=CreateObjectFilter({
   Rule="ANY",
   IncludeThing = {
- "JapanLightTransportVehicle","AlliedNightinaleHelicopter"
+    "JapanLightTransportVehicle",
+  }
+})
+FilterAlliedNightinaleHelicopter=CreateObjectFilter({
+  Rule="ANY",
+  IncludeThing = {
+    "AlliedNightinaleHelicopter",
   }
 })
 
@@ -9,10 +15,18 @@ FilterJapanLightTransportVehicle=CreateObjectFilter({
 function HIDEOBJ ()
   local SELF, count = ObjectFindObjects(nil, nil, FilterJapanLightTransportVehicle)
   for i = 1 , count , 1 do
-  ObjectLoadAttributeModifier(SELF[i], "AttributeMod_ContainedTurretREALLYDAMAGED", 9999)
-  ExecuteAction("UNIT_CHANGE_OBJECT_STATUS",SELF[i],"STEALTHED",1)
-  ExecuteAction("UNIT_CHANGE_OBJECT_STATUS",SELF[i],"UNDER_IRON_CURTAIN",1)
+    ObjectLoadAttributeModifier(SELF[i], "AttributeMod_ContainedTurretREALLYDAMAGED", 9999)
+    ExecuteAction("UNIT_CHANGE_OBJECT_STATUS",SELF[i],"STEALTHED",1)
+    ExecuteAction("UNIT_CHANGE_OBJECT_STATUS",SELF[i],"UNDER_IRON_CURTAIN",1)
   end
 end
 
 HIDEOBJ ()
+-- 只对地图上已有的夜莺生效，后续生产的夜莺不受影响
+local initialNightinales, countNightinales = ObjectFindObjects(nil, nil, FilterAlliedNightinaleHelicopter)
+for i = 1 , countNightinales , 1 do
+  local nightinale = initialNightinales[i]
+  ObjectLoadAttributeModifier(nightinale, "AttributeMod_ContainedTurretREALLYDAMAGED", 9999)
+  ExecuteAction("UNIT_CHANGE_OBJECT_STATUS", nightinale, "STEALTHED", 1)
+  ExecuteAction("UNIT_CHANGE_OBJECT_STATUS", nightinale, "UNDER_IRON_CURTAIN", 1)
+end
